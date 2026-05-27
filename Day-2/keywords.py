1f07bae8c8020d6f5f28a7160d9895de2a7d6259
kumaraswami mandadi <kumarmandadi555@gmail.com>
2026-05-27 17:49:41 +0530

    Add files via upload

---
+++ Day-2/keywords.py
@@ -0,0 +1,52 @@
+Python 3.14.5 (tags/v3.14.5:5607950, May 10 2026, 10:43:50) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+====================================== RESTART: C:/Users/kumar/OneDrive/Desktop/python-coursework/Day-2/keywords.py =====================================
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+35
+Myvar=10
+MYvar1=10
+Myvar@1=10
+SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?
+>>> my_var=10
+>>> my var=10
+SyntaxError: invalid syntax
+>>> myvar=10
+>>> Myvar=10
+>>> myvar
+10
+>>> Myvar
+10
+>>> 1myvar=10
+SyntaxError: invalid decimal literal
+>>> if = 10
+SyntaxError: invalid syntax
+>>> var=10
+>>> Var=10
+>>> var
+10
+>>> Var
+10
+>>> #assigning variable
+>>> a=10
+>>> b=20
+>>> a
+10
+>>> b
+20
+>>> a=b=c=10=20=30
+SyntaxError: cannot assign to literal
+>>> a=b=c=10
+>>> a
+10
+>>> b
+10
+>>> c
+10
+>>> a,b,c=10,20,30
+>>> a
+10
+>>> b
+20
+>>> c
+30
